c87557e324ce2b99bd4461b9758cc787e14b9a2b
Sergey Grigorenko <miraigajettolab@gmail.com>
2020-12-11 06:48:18 +0500

    Create branch for sorcecode developement

--- test/utils/isCharSlashDot.test.js
+++
@@ -1,9 +0,0 @@
-import isCharSlashDot from '../../src/utils/isCharSlashDot';
-
-describe('isCharSlashDot', () => {
-  it('sane default', () => expect(isCharSlashDot()).toBe(false));
-  it('passes parameter tests', () => {
-    expect(isCharSlashDot('・')).toBe(true);
-    expect(isCharSlashDot('/')).toBe(false);
-  });
-});
