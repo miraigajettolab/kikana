2c5c914004ac35fa79561bc6a9fb725239d54614
Sergey Grigorenko <miraigajettolab@gmail.com>
2020-12-11 06:25:50 +0500

    Update App.js

--- demo/try-kikana/src/App.js
+++ demo/try-kikana/src/App.js
@@ -5,15 +5,29 @@ import './App.css';
 
 function App() {
   const [input, setInput] = useState("");
+  const [secondInput, setSecondInput] = useState("");
+  const [englishInput, setEnglishInput] = useState("");
 
   return (
     <div className="App">
-      <input 
+      <input
       type="text"
       value={input}
       onChange={change => setInput(change.target.value) }>
       </input>
       {cyrillicToHiragana(input)}
+      <br></br>
+      <input 
+      type="text"
+      value={cyrillicToHiragana(secondInput)}
+      onChange={change => setSecondInput(change.target.value) }>
+      </input>
+      <br></br>
+      <input 
+      type="text"
+      value={toHiragana(englishInput)}
+      onChange={change => setEnglishInput(change.target.value) }>
+      </input>
     </div>
   );
 }
